dc603b188f74ed1fbc6e27a9aa64c005479dbf54
marshot <124557537+marshot9420@users.noreply.github.com>
2023-03-06 16:18:15 +0900

    3. 버튼에 기능 개발 & state 변경하는 법
    - Warnning 메세지 보기 싫으면
    /* eslint-disable */
    onClick={함수명}
    클릭시 일어나는 EventHandler

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+/* eslint-disable */
+
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   let post = "Title 1";
+  let [title, titles] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "파이썬 독학",
+  ]);
+  let [like, likes] = useState(0);
+
+  const handleLikeClick = () => {
+    likes(like + 1);
+  };
 
   return (
     <div className="App">
@@ -10,7 +23,17 @@ function App() {
         <h4>MARSHOT PROJECT</h4>
       </div>
       <div className="content">
-        <h4>Title</h4>
+        <h4>{title[0]}</h4>
+        <p>Published on Feb. 17th</p>
+        <span onClick={handleLikeClick}>❤</span>
+        <span>{like}</span>
+      </div>
+      <div className="content">
+        <h4>{title[1]}</h4>
+        <p>Published on Feb. 17th</p>
+      </div>
+      <div className="content">
+        <h4>{title[2]}</h4>
         <p>Published on Feb. 17th</p>
       </div>
     </div>
