444852a79a6a45eb151d25953733aff961de5470
marshot <124557537+marshot9420@users.noreply.github.com>
2023-03-06 17:13:14 +0900

    6. 동적인 UI 만드는 법 (모달창 만들기)
    동적인 UI 만드는 Step
    1. HTML CSS로 미리 디자인 완성
    2. UI의 현재 상태를 state로 저장
    3. state에 따라 UI가 어떻게 보일지 작성

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let post = "Title 1";
-  let [title, titles] = useState([
+  let [title, setTitle] = useState([
     "남자 코트 추천",
     "강남 우동 맛집",
     "파이썬 독학",
   ]);
-  let [like, likes] = useState(0);
+  let [like, setLike] = useState(0);
+  let [detail, setDetail] = useState(false);
 
   const handleLikeClick = () => {
-    likes(like + 1);
+    setLike(like + 1);
+  };
+
+  const handleDetailClick = () => {
+    if (detail === false) {
+      setDetail(true);
+    } else {
+      setDetail(false);
+    }
   };
 
   return (
@@ -22,25 +30,29 @@ function App() {
       <div className="black-nav">
         <h4>MARSHOT PROJECT</h4>
       </div>
+
       <div className="content">
-        <h4>{title[0]}</h4>
+        <h4 onClick={handleDetailClick}>{title[0]}</h4>
         <p>Published on Feb. 17th</p>
         <span onClick={handleLikeClick}>❤</span>
         <span>{like}</span>
       </div>
+
       <div className="content">
         <h4>{title[1]}</h4>
         <p>Published on Feb. 17th</p>
         <span onClick={handleLikeClick}>❤</span>
         <span>{like}</span>
       </div>
+
       <div className="content">
         <h4>{title[2]}</h4>
         <p>Published on Feb. 17th</p>
         <span onClick={handleLikeClick}>❤</span>
         <span>{like}</span>
       </div>
-      <ContentDetail></ContentDetail>
+
+      {detail == true ? <ContentDetail /> : null}
     </div>
   );
 }
